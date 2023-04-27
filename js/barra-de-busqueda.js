333afbb2ae029cda251febf6df32d7cc1b220607
luciolazarte9 <lucioagustinlazarte@gmail.com>
2023-04-27 02:24:36 -0300

    Barra de busqueda

--- js/barra-de-busqueda.js
+++ js/barra-de-busqueda.js
@@ -1,43 +1,41 @@
-const juegos = [
-    { titulo: 'Counter Strike 2', genero: 'Shooter', plataforma: 'PC' },
-    { titulo: 'Days Gone', genero: 'Aventura', plataforma: 'PC' },
-    { titulo: 'UFC 4', genero: 'Deportes', plataforma: 'PlayStation' },
-    { titulo: 'Need For Speed', genero: 'Aventura', plataforma: 'PC' },
-    { titulo: 'Warframe', genero: 'Accion', plataforma: 'PC' },
-    { titulo: 'Fifa 23', genero: 'Deportes', plataforma: 'PC' },
-    { titulo: 'APEX Legends', genero: 'Shooter', plataforma: 'PC' },
-    { titulo: 'Metro Exodus', genero: 'Accion, Aventura', plataforma: 'PC' },
-];
+// Seleccionar todos los elementos de juego
+const juegosElements = document.querySelectorAll(".juego");
 
-// Obtener el formulario y el contenedor de resultados
-const formulario = document.querySelector("#formulario");
-const contenedorResultados = document.querySelector("#resultados");
+// Crear un objeto para cada juego
+const juegosObj = Array.from(juegosElements).map(juegoElement => {
+  const nombre = juegoElement.querySelector(".card-title").textContent;
+  const imagen = juegoElement.querySelector(".card-img-top").src;
+  const descripcion = juegoElement.querySelector(".card-text").textContent;
+  return {
+    nombre,
+    imagen,
+    descripcion,
+    getElement: function() {
+      juegoElement.querySelector(".card-title").textContent = this.nombre;
+      juegoElement.querySelector(".card-img-top").src = this.imagen;
+      juegoElement.querySelector(".card-text").textContent = this.descripcion;
+      return juegoElement;
+    }
+  };
+});
 
-// Agregar el evento de búsqueda al formulario
-formulario.addEventListener("submit", function(event) {
-  // Evitar que se envíe el formulario de forma predeterminada
+function buscarJuegos(event) {
   event.preventDefault();
-
-  // Obtener el valor del campo de búsqueda
-  const valorBusqueda = document.querySelector("#searchbar").value.toLowerCase();
-
-  // Filtrar la lista de juegos según el valor de búsqueda
-  const juegosFiltrados = juegos.filter(function(juego) {
-    return juego.titulo.toLowerCase().includes(valorBusqueda) || juego.plataforma.toLowerCase().includes(valorBusqueda);
-  });
-
-  // Mostrar los resultados en la página
-  if (juegosFiltrados.length > 0) {
-    contenedorResultados.innerHTML = "";
-    juegosFiltrados.forEach(function(juego) {
-      const resultado = document.createElement("p");
-      resultado.textContent = `${juego.titulo} - ${juego.plataforma}`;
-      contenedorResultados.appendChild(resultado);
+  const busqueda = document.querySelector("#searchbar").value.toLowerCase().trim();
+  const resultados = juegosObj.filter(juego => juego.nombre.toLowerCase().includes(busqueda));
+  const contenedorResultados = document.querySelector("#resultados");
+  contenedorResultados.innerHTML = "";
+  if (resultados.length > 0) {
+    resultados.forEach(resultado => {
+      contenedorResultados.appendChild(resultado.getElement());
     });
   } else {
-    const mensajeError = document.createElement("p");
-    mensajeError.textContent = "No se encontraron juegos que coincidan con la búsqueda.";
-    contenedorResultados.innerHTML = "";
+    const mensajeError = document.createElement("div");
+    mensajeError.classList.add("alert", "alert-danger");
+    mensajeError.textContent = "No se encontraron resultados";
     contenedorResultados.appendChild(mensajeError);
   }
-});
+}
+
+const formulario = document.querySelector("#formulario");
+formulario.addEventListener("submit", buscarJuegos);
